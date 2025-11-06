cea9e41aee678904f96bdefb6085aea35c9b601b
bernburn <bern.github@gmail.com>
2025-11-06 13:49:40 +0800

    fix2

--- src/main/java/com/musngi/facebookapi/PostController.java
+++ src/main/java/com/musngi/facebookapi/PostController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://post-ui-9f32.onrender.com")
 public class PostController {
 
     @Autowired
